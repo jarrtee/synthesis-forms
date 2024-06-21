6c36b90a2272e49598d40820ec792fa928ba7f89
jarrett <895152506@qq.com>
2024-06-21 15:36:45 +0800

    6/21修改

---
+++ src/Main-forms.vue
@@ -0,0 +1,52 @@
+<!-- <template>
+  <div id="main">
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view/>
+  </div>
+</template> -->
+<template>
+  <div id="main">
+    <el-container>
+      <el-header>
+        <el-menu
+          :default-active="1"
+          class="el-menu-demo"
+          mode="horizontal"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+          @select="handleSelect"
+        >
+          <el-menu-item index="1">Processing Center</el-menu-item>
+          <el-sub-menu index="2">
+            <template #title>Workspace</template>
+            <el-menu-item index="2-1">item one</el-menu-item>
+            <el-menu-item index="2-2">item two</el-menu-item>
+            <el-menu-item index="2-3">item three</el-menu-item>
+            <el-sub-menu index="2-4">
+              <template #title>item four</template>
+              <el-menu-item index="2-4-1">item one</el-menu-item>
+              <el-menu-item index="2-4-2">item two</el-menu-item>
+              <el-menu-item index="2-4-3">item three</el-menu-item>
+            </el-sub-menu>
+          </el-sub-menu>
+          <el-menu-item index="3" disabled>Info</el-menu-item>
+          <el-menu-item index="4">Orders</el-menu-item>
+        </el-menu>
+      </el-header>
+      <el-container>
+        <el-aside width="130px">Aside</el-aside>
+        <el-main>Main</el-main>
+      </el-container>
+    </el-container>
+  </div>
+</template>
+
+
+
+<style lang="less" scoped>
+@import url("./Main-forms.less");
+</style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,16 @@
 import { createApp } from 'vue'
-import App from './App.vue'
+import Main from './Main-forms.vue'
 import router from './router'
 import store from './store'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'  
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/reset.css'
 
-createApp(App).use(store).use(router).mount('#app')
+// const main = createApp(Main)
+// main.use(ElementPlus)
+// main.use(Antd)
+// main.mount('#main')
+
+
+createApp(Main).use(store).use(router).use(ElementPlus).use(Antd).mount('#main')
