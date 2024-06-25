31741f80cf4a01704db62bd1d324ca37466863d3
jarrett <895152506@qq.com>
2024-06-25 15:52:05 +0800

    6/25-1修改

--- src/Main-forms.vue
+++ src/Main-forms.vue
@@ -39,7 +39,7 @@
             <el-button class="button-1" @click="show = !show" circle>
               <el-icon><Search /></el-icon>
             </el-button>
-            <!-- <el-input-group> -->
+            <el-input-group>
               <transition name="fade">
                 <el-input
                   v-model="input"
@@ -50,17 +50,61 @@
                   v-focus
                 />
               </transition>
-              <button
-                v-if="show"
-                class="button-2"
-                @click="enter-click"
-              ></button>
-            <!-- </el-input-group> -->
+              <el-button v-if="show" class="button-2" @click="enterclick"
+                >Search</el-button
+              >
+            </el-input-group>
           </div>
         </el-menu>
       </el-header>
       <el-container>
-        <el-aside width="130px">Aside</el-aside>
+        <el-aside width="130px">
+          <el-row class="row-menu">
+            <el-col :span="25">
+              <el-menu
+                active-text-color="burlywood"
+                background-color="aliceblue"
+                class="el-menu-vertical-demo"
+                default-active="2"
+                text-color="mediumaquamarine"
+                @open="handleOpen"
+                @close="handleClose"
+              >
+                <el-sub-menu index="1">
+                  <template #title>
+                    <el-icon><location /></el-icon>
+                    <span>One</span>
+                  </template>
+                  <el-menu-item-group title="Group One">
+                    <el-menu-item index="1-1">item one</el-menu-item>
+                    <el-menu-item index="1-2">item two</el-menu-item>
+                  </el-menu-item-group>
+                  <el-menu-item-group title="Group Two">
+                    <el-menu-item index="1-3">item three</el-menu-item>
+                  </el-menu-item-group>
+                  <el-sub-menu index="1-4">
+                    <template #title>item four</template>
+                    <el-menu-item index="1-4-1" style="right: 20px"
+                      >item one</el-menu-item
+                    >
+                  </el-sub-menu>
+                </el-sub-menu>
+                <el-menu-item index="2">
+                  <el-icon><icon-menu /></el-icon>
+                  <span>Two</span>
+                </el-menu-item>
+                <el-menu-item index="3">
+                  <el-icon><document /></el-icon>
+                  <span>Three</span>
+                </el-menu-item>
+                <el-menu-item index="4">
+                  <el-icon><setting /></el-icon>
+                  <span>Four</span>
+                </el-menu-item>
+              </el-menu>
+            </el-col>
+          </el-row>
+        </el-aside>
         <el-main>Main</el-main>
       </el-container>
     </el-container>
@@ -68,7 +112,13 @@
 </template>
 
 <script>
-import { Search } from "@element-plus/icons-vue";
+import {
+  Search,
+  Document,
+  Menu as IconMenu,
+  Location,
+  Setting,
+} from "@element-plus/icons-vue";
 
 export default {
   data() {
@@ -79,6 +129,10 @@ export default {
   },
   components: {
     Search,
+    Document,
+    IconMenu,
+    Location,
+    Setting,
   },
   directives: {
     focus: {
@@ -88,7 +142,9 @@ export default {
       },
     },
   },
-  setup() {},
+  setup() {
+    
+  },
 };
 </script>
 
