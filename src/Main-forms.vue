dbb74901c3f79628274abb2d1b1211dd1e8daa3d
jarrett <895152506@qq.com>
2024-07-10 16:19:26 +0800

    7/10-1

--- src/Main-forms.vue
+++ src/Main-forms.vue
@@ -72,22 +72,56 @@
               style="--el-switch-on-color: gray"
             >
               <template #active-action>
-                <el-icon class="icon-Moon"><Moon /></el-icon>
+                <el-icon style="color: midnightblue"><Moon /></el-icon>
               </template>
               <template #inactive-action>
-                <el-icon><Sunny /></el-icon>
+                <el-icon style="color: firebrick"><Sunny /></el-icon>
               </template>
             </el-switch>
           </div>
         </el-menu>
       </el-header>
-      <!-- <el-aside width="130px"> -->
-      <!-- <el-row class="row-menu">
-        <el-col :span="25"> -->
-      <!-- </el-col>
-        </el-row> -->
-      <!-- </el-aside> -->
+      <el-container>
+      <el-aside width="130px" >
+        <el-row class="row-menu">
+          <el-col :span="12">
+            <el-menu
+              default-active="2"
+              class="el-menu-vertical-demo"
+              @open="handleOpen"
+              @close="handleClose"
+            >
+              <el-sub-menu index="1">
+                <template #title>
+                  <el-icon><location /></el-icon>
+                  <span>Navigator One</span>
+                </template>
+                <el-menu-item-group title="Group One">
+                  <el-menu-item index="1-1" style="justify-content: center;">item one</el-menu-item>
+                  <el-menu-item index="1-2" style="justify-content: center;">item two</el-menu-item>
+                </el-menu-item-group>
+                <el-menu-item-group title="Group Two">
+                  <el-menu-item index="1-3" style="justify-content: center;">item three</el-menu-item>
+                </el-menu-item-group>
+              </el-sub-menu>
+              <el-menu-item index="2">
+                <el-icon><icon-menu /></el-icon>
+                <span>Navigator Two</span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <el-icon><document /></el-icon>
+                <span>Navigator Three</span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <el-icon><setting /></el-icon>
+                <span>Navigator Four</span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
+      </el-aside>
       <el-main> </el-main>
+      </el-container>
     </el-container>
   </div>
 </template>
