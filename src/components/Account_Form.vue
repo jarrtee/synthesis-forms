689fbbbbd658dd9e38df049b2dec3abc834dc095
jarrett <895152506@qq.com>
2024-08-13 12:54:40 +0800

    8/13

--- src/components/Account_Form.vue
+++ src/components/Account_Form.vue
@@ -12,7 +12,6 @@
         <span>用户管理</span>
       </div>
     </template>
-    <Vue3Barcode value='125421654'  width='3' height='40' :options="{ format: 'CODE128' , displayValue: false }" style="width: 100px;"></Vue3Barcode>
     <el-radio-group v-model="size" aria-label="size control">
       <el-radio-button value="large">large</el-radio-button>
       <el-radio-button value="default">default</el-radio-button>
@@ -24,12 +23,12 @@
 <script>
 
 // import { ref } from 'vue'
-import Vue3Barcode from 'vue3-barcode'
+
 
 export default {
 
   components: {
-    Vue3Barcode
+    
   },
   setup() {
 
