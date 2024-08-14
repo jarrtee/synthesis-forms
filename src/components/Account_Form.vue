3cb74c17cea7f608e871272267b8542b8bbed1bb
jarrett <895152506@qq.com>
2024-08-14 15:06:12 +0800

    8/14

--- src/components/Account_Form.vue
+++ src/components/Account_Form.vue
@@ -6,6 +6,7 @@
     :modal="false"
     :close-on-click-modal="false"
     class="dialog-form"
+    @close="dialogclose"
   >
     <template #header>
       <div class="custom-header">
@@ -14,20 +15,38 @@
     </template>
     <el-container class="layout-container-demo">
       <el-aside width="100px">
-        <el-scrollbar>
-          <el-menu
-          :default-active="activeIndex"
-          class="el-menu-demo"
-          mode="horizontal"
-          @select="handleSelect"
-          :ellipsis="false"
-          >
-          <el-menu-item index="1" class="menu-item-1">
-            <span class="title-label-1"><strong>主页主页</strong></span>
-            <el-icon class="title-icon-1"><HomeFilled /></el-icon>
-          </el-menu-item>
-          </el-menu>
-        </el-scrollbar>
+        <el-row class="row-menu">
+          <el-col :span="24">
+            <el-menu
+              :default-active="defaultactive"
+              class="el-menu-vertical-demo"
+              @select="indexselect"
+              @open="handleOpen"
+              @close="handleClose"
+            >
+              <el-menu-item index="1">
+                <el-icon class="left-icon-1"><EditPen /></el-icon>
+                <span class="left-label-1"><strong>基础信息</strong></span>
+              </el-menu-item>
+              <el-menu-item index="2">
+                <el-icon class="left-icon-1"><User /></el-icon>
+                <span class="left-label-1"><strong>添加用户</strong></span>
+              </el-menu-item>
+              <el-menu-item index="3">
+                <el-icon class="left-icon-1"><Memo /></el-icon>
+                <span class="left-label-1"><strong>权限管理</strong></span>
+              </el-menu-item>
+              <el-menu-item index="4">
+                <el-icon class="left-icon-1"><Bell /></el-icon>
+                <span class="left-label-1"><strong>通知</strong></span>
+              </el-menu-item>
+              <el-menu-item index="5">
+                <el-icon class="left-icon-1"><Setting /></el-icon>
+                <span class="left-label-1"><strong>设置</strong></span>
+              </el-menu-item>
+            </el-menu>
+          </el-col>
+        </el-row>
       </el-aside>
       <el-main>Main</el-main>
     </el-container>
@@ -35,24 +54,47 @@
 </template>
 
 <script>
-import {
-  HomeFilled,
-} from "@element-plus/icons-vue";
+import { Setting, User, EditPen, Memo, Bell } from "@element-plus/icons-vue";
+import { ref } from "vue";
 
 export default {
+  date() {
+    return {
+      
+    };
+  },
   components: {
-    HomeFilled
+    Setting,
+    User,
+    EditPen,
+    Memo,
+    Bell,
   },
   setup() {
-    return {};
+
+    const dialogclose = () => {
+      defaultactive.value = '1'
+    };
+
+    const indexselect = (index) =>{
+      defaultactive.value = index
+    }
+
+    const defaultactive = ref('1');
+
+    return {
+      defaultactive,
+      dialogclose,
+      indexselect,
+    };
   },
 };
 </script>
 
 <style lang="less" scoped>
-.Vue3Barcode path,
-.Vue3Barcode rect {
-  overflow: visible;
-}
+// .Vue3Barcode path,
+// .Vue3Barcode rect {
+//   overflow: visible;
+// }
 @import url("./Less/Account_From.less");
 </style>
